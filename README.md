3ac1a74a22abd9cb3a2f89f1ec9411ba974e3d34
Jhon Nova <jhonnovax@gmail.com>
2024-11-28 15:30:00 -0800

    Added readme and sql performances based on indexes

--- README.md
+++ README.md
@@ -2,3 +2,9 @@
 
 - Set profiling
 - Query measures with database indexes
+- MySql Credentials
+	- Host: csislinux1.douglascollege.ca
+	- Port: 23306
+	- User: csis3300
+	- Pass: dkt75gT
+
--- sql indexes.sql
+++ sql indexes.sql
@@ -1,9 +1,19 @@
 SET profiling = 1;
 
+explain select * from employees.employees
+where first_name = 'Berni';
+
+select * from employees.employees
+where first_name = 'Berni';
+
 select sql_no_cache * from employees.employees
 where first_name = 'Berni';
 
-select sql_no_cache * from emp1.employees
+# Query performances
+select * from employees.employees
+where first_name = 'Sluis';
+
+select * from emp1.employees
 where first_name = 'Sluis';
 
 
