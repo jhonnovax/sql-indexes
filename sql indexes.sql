05341cdf04b18eba09e795500651c08f7c8baa77
Jhon Nova <jhonnovax@gmail.com>
2024-11-28 18:42:55 -0800

    Added sql subqueries

--- sql indexes.sql
+++ sql indexes.sql
@@ -3,7 +3,8 @@ SET profiling = 1;
 explain select * from employees.employees
 where first_name = 'Berni';
 
-select * from employees.employees
+
+explain select sql_no_cache * from emp1.employees
 where first_name = 'Berni';
 
 select sql_no_cache * from employees.employees
@@ -13,8 +14,29 @@ where first_name = 'Berni';
 select * from employees.employees
 where first_name = 'Sluis';
 
-select * from emp1.employees
+explain select * from emp1.employees
 where first_name = 'Sluis';
 
 
 show profiles;
+
+
+
+select * 
+from employees.dept_emp;
+
+select *
+from employees.departments;
+
+explain select * 
+from employees.employees e
+left join employees.dept_emp de on e.emp_no = de.emp_no
+left join employees.departments d on de.dept_no = d.dept_name
+and d.dept_name = 'Customer Service';
+
+explain select * 
+from employees.departments d
+left join employees.dept_emp de on de.dept_no = d.dept_no 
+left join employees.employees e on e.emp_no = de.emp_no
+where d.dept_name = 'Customer Service'
+
